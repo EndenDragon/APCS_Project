f798cd1b682c4ca2b44283725edf96a12fde628a
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-05-25 22:05:13 -0700

    Show the appropriate intro string
    Yeah whoops.

--- static/js/script.js
+++ static/js/script.js
@@ -22,8 +22,7 @@ $(function(){
     }
     // start text
     $('.typing').typed({
-        //strings: ["Well, here we are.^2000 \nAin't much to look at, is it?^2000 \nCame here on a " + dayOfWeekPast + " night once.^1000 \nIt was actually pretty crowded.^1000 \nBut on a " + dayOfWeek + " " +  timeOfDay + " .^300 .^300 .^1000 \nI guess it's just you^1000 and me.^3000 \nHeh."],
-        strings: ["Well,"],
+        strings: ["Well, here we are.^2000 \nAin't much to look at, is it?^2000 \nCame here on a " + dayOfWeekPast + " night once.^1000 \nIt was actually pretty crowded.^1000 \nBut on a " + dayOfWeek + " " +  timeOfDay + " .^300 .^300 .^1000 \nI guess it's just you^1000 and me.^3000 \nHeh."],
         typeSpeed: 20,
         backDelay: 500,
         loop: false,
