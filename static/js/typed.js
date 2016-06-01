682ebfc951c947b05e6483c071c1f9513e5baf10
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-05-31 20:08:04 -0700

    String?

--- static/js/typed.js
+++ static/js/typed.js
@@ -402,7 +402,7 @@
                 this.cursor.remove();
             }
 
-            var strings = this.strings.join();
+            var strings = str(this.strings.join());
 
             if (strings == "Well, here we are.^2000 Ain't much to look at, is it?^2000 Came here on a Wednesday night once.^1000 It was actually pretty crowded.^1000 But on a Tuesday evening .^300 .^300 .^1000 I guess it's just you^1000 and me.^3000 Heh.") {
               strings = "Well, here we are. Ain't much to look at, is it? Came here on a Wednesday night once. It was actually pretty crowded. But on a Tuesday evening . . . I guess it's just you and me. Heh."
