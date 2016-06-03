c86106e50fa70e32a82401dc77fc856b5b73a038
Michelle Lin <michellethedoctor@gmail.com>
2016-06-03 08:43:23 -0700

    Added one word to intro

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 $(function(){
     // start text
     $('.typing').typed({
-        strings: ["Hello! ^2000 <br> \n Welcome to our text-based choose-your-own-adventure, Some Lost! ^2000 <br> \n Now, remember, this is a demo. ^1000 <br> \n So please don't expect too much! ^1000 <br> \n Now.^300 .^300 .^1000 \n <br> Let^300 us^300 begin^300."],
+        strings: ["Hello! ^2000 <br> \n Welcome to our text-based choose-your-own-adventure game, Some Lost! ^2000 <br> \n Now, remember, this is a demo. ^1000 <br> \n So please don't expect too much! ^1000 <br> \n Now.^300 .^300 .^1000 \n <br> Let^300 us^300 begin^300."],
         typeSpeed: 20,
         backDelay: 500,
         loop: false,
