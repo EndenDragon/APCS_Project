a581be10c60c0785b281143886eb51163af5ea8c
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-06-07 09:57:13 -0700

    Catch error and rerun function if error

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ var isIntroPlaying = true; //This variable shows weather the intro is currently 
 $(function(){
     // start text
     $('.typingIntro').typed({
-        strings: ["                    Hello! ^2000 <br> \n Welcome to our text-based choose-your-own-adventure game, Some Lost! ^2000 <br> \n Now, remember, this is a demo. ^1000 <br> \n So please don't expect too much! ^1000 <br> \n Now.^300 .^300 .^1000 \n <br> Let^300 us^300 begin^300."],
+        strings: ["Hello! ^2000 <br> \n Welcome to our text-based choose-your-own-adventure game, Some Lost! ^2000 <br> \n Now, remember, this is a demo. ^1000 <br> \n So please don't expect too much! ^1000 <br> \n Now.^300 .^300 .^1000 \n <br> Let^300 us^300 begin^300."],
         typeSpeed: 30,
         backDelay: 500,
         loop: false,
@@ -19,34 +19,40 @@ function startStory(){
 }
 
 function showStory(int) {
-  console.log("Show story: " + int);
   var currentDomain = window.location.protocol + "//" + window.location.host + "/";
-  $.getJSON(currentDomain + "storyline/" + int, function(data) {
-    var buttonElements = document.getElementById("kick");
-    buttonElements.innerHTML = '';
-    if (int != 1) {
-      $(".typing").typed('nextLine');
-    } //Row 1 is the beginning of the story, we dont need to have it make a new line for us.
+  $.ajax({
+    url: currentDomain + "storyline/" + int,
+    dataType: 'json',
+    success: function( data ) {
+      var buttonElements = document.getElementById("kick");
+      buttonElements.innerHTML = '';
+      if (int != 1) {
+        $(".typing").typed('nextLine');
+      } //Row 1 is the beginning of the story, we dont need to have it make a new line for us.
 
-    $('.typing').typed({
-        strings: [data["story"]],
-        typeSpeed: 30,
-        backDelay: 500,
-        loop: false,
-        loopCount: false,
-        callback: showButtons(data["btn1_txt"], data["btn1_loc"], data["btn2_txt"], data["btn2_loc"], data["btn3_txt"], data["btn3_loc"]),
-    });
-	
-	if (data["quicktimeplayer_enabled"] == 1) {
-		setTimeout(function(){
-			showStory(data["quicktimeplayer_nextoption"]);
-		}, data["quicktimeplayer_seconds"] * 1000);
-	}
-		 //TODO: Edit MySQL Database so it would go to the proper next row (Instead of the assumed next row) if the player didnt press it in time.
-	
-    
+      $('.typing').typed({
+          strings: [data["story"]],
+          typeSpeed: 30,
+          backDelay: 500,
+          loop: false,
+          loopCount: false,
+          callback: showButtons(data["btn1_txt"], data["btn1_loc"], data["btn2_txt"], data["btn2_loc"], data["btn3_txt"], data["btn3_loc"]),
+      });
+  	
+  	if (data["quicktimeplayer_enabled"] == 1) {
+  		setTimeout(function(){
+  			showStory(data["quicktimeplayer_nextoption"]);
+  		}, data["quicktimeplayer_seconds"] * 1000);
+  	}
+  		 //TODO: Edit MySQL Database so it would go to the proper next row (Instead of the assumed next row) if the player didnt press it in time.
+
+    },
+    error: function( data ) {
+      showStory(int);
+    }
   });
 }
+
 function showButtons(btn1_txt, btn1_loc, btn2_txt, btn2_loc, btn3_txt, btn3_loc) {
   if (btn1_txt != null) {
     $('#kick').append(buttonGen(btn1_txt, btn1_loc));
